7ff540b40412af82e4c61c94a193e779f706b3ba
AbineshKThatchinamoorthy <77613419+AbineshKThatchinamoorthy@users.noreply.github.com>
2021-03-25 22:27:30 +0530

    Update SensorValidation.c

--- SensorValidation.c
+++ SensorValidation.c
@@ -8,19 +8,10 @@
 *\n*/
 /*****************************************************************************/
 
+/*=============================================================================
+=======                             INCLUSIONS                          =======
+=============================================================================*/
 #include "SensorValidation.h"
-
-int validateSensorIp_i(double* sensorVals_a, int lastButOneIndex_i, double maxDelta) {
-   for(int cnt_i = 0; cnt_i < lastButOneIndex_i; cnt_i++) {
-      if(sensorVals_a[cnt_i + 1] - sensorVals_a[cnt_i] > maxDelta) {
-        return 0;
-      }
-      else if(sensorVals_a[cnt_i + 1] - sensorVals_a[cnt_i] > maxDelta) {
-        return 0; 
-      }
-    }
-  return 1;
-}
 
 /*---------------------------------------------------------------------------*/
 /*     FUNCTION:  validateSensorReadings_i
@@ -43,4 +34,45 @@ int validateSensorReadings_i(double* sensorVals_a, int numOfVals_i, double maxDe
   }
   
   return retVal_i;
+}
+
+/*---------------------------------------------------------------------------*/
+/*     FUNCTION:  iterateSensorReadings_i
+ */
+/*!    \brief     Iterate each sensor and send it for validation
+ *
+ *     \param     double* sensorVals_a - array of sensor values
+ *     \param     int lastButOneIndex_i - (No of sensor values - 1)
+ *     \param     double maxDelta_d - maximum delta for the corr. batt. param
+ *     \returns   1 - if all the inputs are valid
+ *                0 - if 1 or more inputs are invalid
+*//*------------------------------------------------------------------------*/
+int iterateSensorReadings_i(double* sensorVals_a, int lastButOneIndex_i, double maxDelta_d) {
+  for(int cnt_i = 0; cnt_i < lastButOneIndex_i; cnt_i++) {
+    if(!validateSensorIp_i(sensorVals_a[cnt_i], sensorVals_a[cnt_i + 1], maxDelta_d)) {
+      return 0;
+    }
+  }
+  return 1;
+}
+
+/*---------------------------------------------------------------------------*/
+/*     FUNCTION:  validateSensorIp_i
+ */
+/*!    \brief     Validate received sensor input against the previous one
+ *
+ *     \param     double value - previous sensor value
+ *     \param     double value - sensor value to be validated
+ *     \param     double maxDelta_d - maximum delta for the corr. batt. param
+ *     \returns   1 - if the input is valid
+ *                0 - if the input is invalid
+*//*------------------------------------------------------------------------*/
+int validateSensorIp_i(double value, double nextValue, double maxDelta) {
+  if(nextValue - value > maxDelta) {
+    return 0;
+  }
+  else if(value - nextValue > maxDelta) {
+    return 0; 
+  }
+  return 1;
 }
