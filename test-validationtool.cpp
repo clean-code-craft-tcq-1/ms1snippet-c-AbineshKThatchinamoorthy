769fbcc4b5788d0ca25b63c6c25558d96f953f8c
AbineshKThatchinamoorthy <77613419+AbineshKThatchinamoorthy@users.noreply.github.com>
2021-03-25 19:02:43 +0530

    Update test-validationtool.cpp

--- test-validationtool.cpp
+++ test-validationtool.cpp
@@ -3,8 +3,20 @@
 #include "test/catch.hpp"
 #include "SensorValidation.h"
 
-TEST_CASE("reports error when soc jumps abruptly") {
+TEST_CASE("reports error when soc increases abruptly") {
   double socReadings[] = {0.0, 0.01, 0.5, 0.51};
+  int numOfSocReadings = sizeof(socReadings) / sizeof(socReadings[0]);
+  REQUIRE(validateSensorReadings_i(socReadings, numOfSocReadings, SOC_DIFF_THRESHOLD) == 0);
+}
+
+TEST_CASE("reports error when soc decreases abruptly") {
+  double socReadings[] = {0.02, 0.06, 0.0, 0.02};
+  int numOfSocReadings = sizeof(socReadings) / sizeof(socReadings[0]);
+  REQUIRE(validateSensorReadings_i(socReadings, numOfSocReadings, SOC_DIFF_THRESHOLD) == 0);
+}
+
+TEST_CASE("reports error when soc varies normally") {
+  double socReadings[] = {0.02, 0.06, 0.04, 0.051};
   int numOfSocReadings = sizeof(socReadings) / sizeof(socReadings[0]);
   REQUIRE(validateSensorReadings_i(socReadings, numOfSocReadings, SOC_DIFF_THRESHOLD) == 0);
 }
