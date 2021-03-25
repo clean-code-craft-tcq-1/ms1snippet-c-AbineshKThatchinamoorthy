6ce7e88168ead7f86c6ec2a0040487a7ffa316f7
AbineshKThatchinamoorthy <77613419+AbineshKThatchinamoorthy@users.noreply.github.com>
2021-03-25 19:06:13 +0530

    Update test-validationtool.cpp

--- test-validationtool.cpp
+++ test-validationtool.cpp
@@ -15,15 +15,26 @@ TEST_CASE("reports error when soc decreases abruptly") {
   REQUIRE(validateSensorReadings_i(socReadings, numOfSocReadings, SOC_DIFF_THRESHOLD) == 0);
 }
 
-TEST_CASE("reports error when soc varies normally") {
+TEST_CASE("Test for no error when soc varies normally") {
   double socReadings[] = {0.02, 0.06, 0.04, 0.051};
   int numOfSocReadings = sizeof(socReadings) / sizeof(socReadings[0]);
-  REQUIRE(validateSensorReadings_i(socReadings, numOfSocReadings, SOC_DIFF_THRESHOLD) == 0);
+  REQUIRE(validateSensorReadings_i(socReadings, numOfSocReadings, SOC_DIFF_THRESHOLD) == 1);
 }
 
-TEST_CASE("reports error when current jumps abruptly") {
+TEST_CASE("reports error when current increases abruptly") {
   double currentReadings[] = {0.0, 0.02, 0.03, 0.33};
   int numOfCurReadings = sizeof(currentReadings) / sizeof(currentReadings[0]);
   REQUIRE(validateSensorReadings_i(currentReadings, numOfCurReadings, CURRENT_DIFF_THRESHOLD) == 0);
 }
 
+TEST_CASE("reports error when current decreases abruptly") {
+  double currentReadings[] = {0.0, 0.1, 0.2, 0.0};
+  int numOfCurReadings = sizeof(currentReadings) / sizeof(currentReadings[0]);
+  REQUIRE(validateSensorReadings_i(currentReadings, numOfCurReadings, CURRENT_DIFF_THRESHOLD) == 0);
+}
+
+TEST_CASE("Test for no error when current increases abruptly") {
+  double currentReadings[] = {0.0, 0.02, 0.03, 0.12};
+  int numOfCurReadings = sizeof(currentReadings) / sizeof(currentReadings[0]);
+  REQUIRE(validateSensorReadings_i(currentReadings, numOfCurReadings, CURRENT_DIFF_THRESHOLD) == 0);
+}
